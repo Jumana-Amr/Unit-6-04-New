b5b3e5cf810a180c1deb75cb8802310f4f383402
JumanaAmr <jumana.amr@ocsbstudent.ca>
2021-02-23 20:06:34 +0000

    Trial 4

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function calculateArea () { // eslint-disable-line no-unused-vars
-  let area = 0;
+  let area = 0
   const base = document.getElementById('id-base').value
   const height = document.getElementById('id-height').value
   area = base * height / 2
