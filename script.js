911675507a71c89feb293025d2639de7aba429ff
JumanaAmr <jumana.amr@ocsbstudent.ca>
2021-02-23 19:52:44 +0000

    Trial 2

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-function calculateArea () {
+function calculateArea () { // eslint-disable-line no-unused-vars
   let area = 0;
-  let base = document.getElementById("id-base").value;
-  let height = document.getElementById("id-height").value
+  const base = document.getElementById('id-base').value;
+  const height = document.getElementById('id-height').value
   area = base * height / 2;
-  document.getElementById("id-output").innerHTML = "Area = " + area;
+  document.getElementById('id-output').innerHTML = 'Area = ' + area;
 }
